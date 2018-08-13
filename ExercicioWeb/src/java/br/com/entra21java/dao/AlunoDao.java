8b2d1e5ad7a471e664718c281fd5a99bd15c39ac
Guilherme Bialas <38558633+GuilhermeBialas@users.noreply.github.com>
2018-08-13 11:23:36 -0300

    exercicioweb

--- ExercicioWeb/src/java/br/com/entra21java/dao/AlunoDao.java
+++ ExercicioWeb/src/java/br/com/entra21java/dao/AlunoDao.java
@@ -1,10 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.com.entra21java.dao;
 
+import br.com.entra21java.bean.AlunoBean;
+import br.com.entra21java.database.Conexao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -12,7 +10,7 @@ import java.util.List;
 
 /**
  *
- * @author Alunos
+ * @author Guilherme Bialas
  */
 public class AlunoDao {
     
--- ExercicioWeb/src/java/br/com/entra21java/web/Aluno/AlunoCadastro.java
+++ ExercicioWeb/src/java/br/com/entra21java/web/Aluno/AlunoCadastro.java
@@ -1,18 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.entra21java.web.Aluno;
 
 import java.io.IOException;
+import static java.lang.System.out;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-
 
 /**
  *
@@ -24,6 +18,45 @@ public class AlunoCadastro extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("	<title>Alunos - Cadastro</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("	<form>");
+        out.println("    <div id='campo-nome'>");
+        out.println("        <label for='campo-nome'>Nome</label>");
+        out.println("  	    <input type='text' name='nome' id='campo-nome'>");
+        out.println("  	</div>");
+        out.println("  	");
+        out.println("  	<div id='codigo_matricula'></div>");
+        out.println("  	    <label>Codigo da Matricula</label>");
+        out.println("        <input type='number' name='codigo_matricula'>");
+        out.println("    </div>");
+        out.println("");
+        out.println("    <div id='nota_1'>");
+        out.println("        <label>Nota 1</label>");
+        out.println("        <input type='number' name='nota_1'>");
+        out.println("    </div>");
+        out.println("");
+        out.println("    <div id='nota_2'>");
+        out.println("        <label>Nota 2</label>");
+        out.println("        <input type='number' name='nota_2'>");
+        out.println("    </div>");
+        out.println("");
+        out.println("    <div id='nota_3'>");
+        out.println("        <label>Nota 3</label>");
+        out.println("        <input type='number' name='nota_3'>");
+        out.println("    </div>");
+        out.println("");
+        out.println("    <div id='frequencia'>");
+        out.println("        <label>Frequencia</label>");
+        out.println("        <input type='number' name='frequencia'>");
+        out.println("    </div>");
+        out.println(" </form>");
+        out.println(" </body>");
+        out.println("</html>");
     }
     
    
